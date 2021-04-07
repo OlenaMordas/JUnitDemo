fe8f756b035608d7b9a8beab7e5adf8253864d0f
olena.mordas <olena.mordas@synpulse.com>
2021-04-07 20:48:59 +0200

    before/after each/all

--- test/com/example/MyMathTest.java
+++ test/com/example/MyMathTest.java
@@ -1,18 +1,36 @@
 package com.example;
 
 import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.BeforeClass;
 import org.junit.jupiter.api.*;
 class MyMathTest {
 
     MyMath myMath = new MyMath();
 
+    @BeforeEach
+    public void before(){
+        System.out.println("BEFORE");
+    }
+
+    @AfterEach
+    public void after(){
+        System.out.println("AFTER");
+    }
+
+    @BeforeAll
+    public static void beforeAll(){
+        System.out.println("BEFORE ALL");
+    }
     @Test
     void sum_with3nums() {
+        System.out.println("Test1");
         assertEquals(6, myMath.sum(new int[] {1,2,3}));
     }
 
     @Test
     void sum_with1num(){
+        System.out.println("Test2");
         assertEquals(7,myMath.sum(new int[] {7}));
     }
 }
